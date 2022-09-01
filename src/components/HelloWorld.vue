623f0fcdc5d658dc8ef7e920c67a1aa32f7a9f25
Alireza Safari <alireza.safaree@gmail.com>
2022-09-01 11:44:49 +0430

    fix(repo): adds localizations

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,9 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Testing Localizations: {{ $t('works-fine') }}
     </p>
   </div>
 
@@ -31,8 +21,32 @@ const count = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  mounted() {},
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.read-the-docs {
-  color: #888;
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
+import en from './locales/en.json'
+import fa from './locales/fa.json'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'fa',
+  messages: {
+    en,
+    fa
+  },
+})
+
+app.use(i18n)
+app.mount('#app')
