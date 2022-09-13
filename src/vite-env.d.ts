55660156820de6eaf90ece7e1e79aeac8ffbaebf
Alireza Safari <alireza.safaree@gmail.com>
2022-09-13 11:08:45 +0430

    Revert "fix(log): fixes logger type issues"
    This reverts commit 7690498fc878348c9c7765ae7f92a37c17c35aa0.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,18 +1,7 @@
 /// <reference types="vite/client" />
 
-import axios from 'axios'
-import { ILogger } from '../src/interfaces/logger'
-import type { DefineComponent } from 'vue'
 declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-}
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $const: Record<string, unknown>
-    logger: typeof ILogger
-    $http: typeof axios
-    $translate: (key: string) => string
-  }
 }
